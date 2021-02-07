82374175279f29587ad014e448fbcc81770a104a
leo <l.louis@outlook.fr>
2021-02-07 08:27:08 +0100

    for fixed

--- app/src/Controller/RegisterController.php
+++ app/src/Controller/RegisterController.php
@@ -2,33 +2,33 @@
 
 namespace App\Controller;
 
+use App\Entity\customers;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+
 use Symfony\Component\Serializer\Exception\NotEncodableValueException;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
-class RegisterController extends AbstractController
+class RegisterController
 {
-    /**
-     * @Route("/register", name="register")
-     */
 
-        public function index(Request $request, SerializerInterface $serializer, EntityManagerInterface $em, ValidatorInterface $validator )
+ /*
+  * @Route(name="api_posts_collection_post", methods={"POST"})
+  */
+    public function post(Request $request, SerializerInterface $serializer, EntityManagerInterface $em, ValidatorInterface $validator )
     {
         $jsonRecu = $request->getContent();
         try {
             $post= $serializer->deserialize($jsonRecu, Post::class, 'json');
             $Erros = $validator->validate($post);
-            if(cont($Erros)>0){
+            if(count($Erros)>0){
                 return $this->json($Erros, 400);
             }
             $em->persist($post);
             $em->flush();
-            return $this->json($post, 201, [], ['groups'=>'post:read']);
+            return $this->json($post, 201, [], ['Groups({"readE","Effectif_details_read"})']);
         }catch (NotEncodableValueException $e){
             return $this->json(
                 [ 'status: '=>400,
